15441b31bfbfcd91348fae29c728d16af8194a8b
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-24 17:00:51 -0700

    commit message

---
+++ exercises/hello-class/app.js
@@ -0,0 +1,8 @@
+var name = "Olivia";
+var occupation = "student";
+
+function sayHello(name, occupation){
+    console.log("Hello " + name + "! How do you like being a " + occupation + "?");
+}
+
+sayHello(name, occupation);
