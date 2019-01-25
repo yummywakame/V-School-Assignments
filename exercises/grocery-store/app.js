c31912ec03eedf2b4fa1023dd673644fa420652b
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-24 17:16:45 -0700

    grocery store app added

---
+++ exercises/grocery-store/app.js
@@ -0,0 +1,18 @@
+var shopper = {
+    firstName: "Olivia",
+    lastName: "Meiring",
+    shopperID: 2100198,
+    isMember: true,
+    fullname: function() {
+        return(this.firstName + " " + this.lastName + "!");
+    },
+    groceryCart: ["apples", "oranges", "bananas", "eggs"]
+ }
+ 
+ console.log("Hello " + shopper.fullname());
+ if (shopper.isMember) {
+    console.log("Thank you for being a member.")
+ }
+ console.log("Your grocery cart contains:")
+ console.log(shopper.groceryCart);
+ 
