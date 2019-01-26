a37e979a509814494580b9048ab4b41b8fa74057
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-25 18:52:32 -0700

    Create readme.js

---
+++ exercises/hello-world-jokes/readme.js
@@ -0,0 +1,7 @@
+# Hello World, Jokes
+### > For V School // Full Stack JavaScript // January 2019 Cohort
+
+DEMO: https://github.com/yummywakame/assignments/blob/master/exercises/hello-world-jokes/index.html](https://github.com/yummywakame/assignments/blob/master/exercises/hello-world-jokes/index.html)
+
+Completed according to assignment instructions: 
+- https://coursework.vschool.io/hello-world-jokes/
