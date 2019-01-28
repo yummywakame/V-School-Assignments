59f2a7afa43d07d9228cc1f8e19c1eb4bb496d6b
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-28 14:23:52 -0700

    fixed social.js

--- exercises/week-02/02-social.js/social.js
+++ exercises/week-02/02-social.js/social.js
@@ -1,8 +1,35 @@
 var member = {
-    firstName = "Bob",
-    lastName = "Builder",
-    occupation = "Builder",
-    pets = ["cats", "dogs", "birds", "reptiles"],
-    favorites = [],
-    
+    firstName:"Mario",
+    lastName: "Bro",
+    occupation: "Adventurer",
+    pets: [
+        {
+            name: "Toad",
+            animal: "cat",
+            breed: "Domestic shorthair",
+            pedigree: true,              
+            birthday: "1/1/2017",
+            favoriteFoods: [
+                {
+                    brand: "Iams",
+                    name: "Organic Steering Control",
+                    count: 3
+                }
+            ]
+        }
+    ]
 }
+
+// Add properties to one of the objects
+member.badges = 2;
+member.addresses = [{}];
+
+// Add items to one of the arrays
+member.addresses[0].line1 = "56 Rainbow Road";
+member.addresses[0].line2 = "";
+member.addresses[0].city = "Marioville";
+member.addresses[0].state = "CA";
+member.addresses[0].zip = 91364;
+member.addresses[0].country = "USA";
+
+console.log(member);
