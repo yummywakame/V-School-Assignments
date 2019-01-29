a7bae215efe46dc00c1f75a9557c9d5db8cb8d3e
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-29 09:15:36 -0700

    morning warmup

---
+++ exercises/week-02/06-tuesday-morning-warmup/instructions.js
@@ -0,0 +1,35 @@
+/*
+    1. Write a function that takes a string as an parameter/argument and returns
+    that string uppercased.
+*/
+
+function upperCaseMe(str){
+    // Your code here
+}
+
+console.log(upperCaseMe("good morning!")) // GOOD MORNING!
+
+
+
+/*
+    2. Write a function that takes a string and returns that string reversed.
+*/
+
+function reverseMe(str){
+    // Your code here
+}
+
+console.log(reverseMe("What do I spell backwards?")) // ?sdrawkcab lleps I od tahW
+
+
+
+/*
+    3. Write a function that takes a string and a number.
+    Return the string slice from index 0 to that argument number.
+*/
+
+function sliceMe(str, num){
+    // Your code here
+}
+
+console.log(sliceMe("This is the string to slice.", 10)) // This is th
---
+++ exercises/week-02/06-tuesday-morning-warmup/readme.md
@@ -0,0 +1,16 @@
+# Tuesday Morning Warmup
+### > For V School // Full Stack JavaScript // January 2019 Cohort
+
+#### Completed according to assignment instructions: 
+- instructions.js
+
+*Run:* ~$ node <a href="https://github.com/yummywakame/assignments/blob/master/exercises/loop-olympics/app.js">app.js</a>
+
+#### Console:
+
+```console
+foo@bar:~/assignments/exercises/week-02/06-tuesday-morning-warmup (master) $ node app.js
+GOOD MORNING!
+?sdrawkcab lleps I od tahW
+This is th
+```
