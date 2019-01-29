3145c7e4661481f57616a2b9fd4915399d21589b
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-28 19:55:44 -0700

    more functions added

--- exercises/week-02/03-functions/app.js
+++ exercises/week-02/03-functions/app.js
@@ -48,9 +48,56 @@ function stringTwerk(str) {
 }
 console.log("\n4: Twerk the string - '" + myStr + "':\nA: " + stringTwerk(myStr));
 
-// BONUS 1: Write a function that accepts a number ‘n’ as a parameter. 
-//          Then print the first ‘n’ Fibonacci numbers and return their sum.
 
-// BONUS 2: Write a function that accepts a string as a parameter. 
-//          Return the most frequently occuring letter in that string. 
-//          ( White spaces count as a letter )
+/* 
+    BONUS 1: Write a function that accepts a number ‘n’ as a parameter. 
+    Then print the first ‘n’ Fibonacci numbers and return their sum.
+*/
+var n = 12
+
+// Basic Fibonacci recursive function
+function fibonacci(n) {
+    if (n <= 1) return 1;
+  
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// First find the number in the 'n' position of the fibonacci sequence
+console.log("\n5: The number in position " + n + " of the fibonacci sequence = " + fibonacci(n - 1))
+
+// Create an array with all the numbers in sequence up to n
+function fibonacciArray(n) {
+    var fibArray = []
+
+    for (i = 0; i < n; i++) {
+      fibArray[i] = fibonacci(i)
+    }
+    return(fibArray)
+}
+console.log("   Fibonacci sequence for n = " + n + ": " + fibonacciArray(n))
+
+// Now add them all up
+var reducer = (accumulator, currentValue) => accumulator + currentValue
+console.log("   The sum of the first " + n + " numbers = " + fibonacciArray(n).reduce(reducer))  
+
+
+/* 
+    BONUS 2: Write a function that accepts a string as a parameter. 
+    Return the most frequently occuring letter in that string. 
+    ( White spaces count as a letter )
+*/
+var myStr = "Sassafrass"
+
+// convert to uppercase and split to an array
+myStr = myStr.toUpperCase().split()
+
+// find the letter with the most occurrances
+function getMax(str) {
+    var letterCount = [[]]
+    var row = 0;
+    var col = 0;
+    
+    return(str)
+}
+console.log("\n\n6: The most frequent letter in the string '" + myStr + "' is: ")
+console.log("A: " + getMax(myStr))
