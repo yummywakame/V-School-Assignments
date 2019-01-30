55d3622bea1b6d9cd2d680634187d1a56b4c8ee1
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-30 15:01:15 -0700

    Create readme.js

---
+++ exercises/week-02/11-dj-js/readme.js
@@ -0,0 +1,8 @@
+# DJ JS
+### > For V School // Full Stack JavaScript // January 2019 Cohort
+
+#### Completed according to assignment instructions: 
+- https://coursework.vschool.io/dj-js/
+
+#### Demo:
+- <a href="http://htmlpreview.github.com/?https://github.com/yummywakame/assignments/blob/master/exercises/newsies/index.html" target="_blank">Newsies</a>
