45a6c6281aa73f2d2b22b860ae935e3011a145e4
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-31 14:23:23 -0700

    form exercises

--- exercises/week-02/12-dom-events-practice/events/submit/app.js
+++ exercises/week-02/12-dom-events-practice/events/submit/app.js
@@ -1,3 +1,14 @@
 // The "submit" event refreshed your brower by default
 // use "e.preventDefault" in the first line of your callback function to prevent this
 // remember to pass "e" into the callback function
+
+var myForm = document["submit-me"]
+var myName = myForm.name
+var myAge = myForm.age
+
+myForm.addEventListener("submit", showFullName)
+
+function showFullName(event) {
+    event.preventDefault()
+    alert("Hi, " + myName.value + "! You are " + myAge.value + " years old.")
+}
