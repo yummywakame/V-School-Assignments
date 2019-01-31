f6957f70ac7dc5eaa3115592431e603d9dddc085
Olivia Meiring <olivia@yummy-wakame.com>
2019-01-31 12:27:04 -0700

    x y coords

--- exercises/week-02/12-dom-events-practice/events/mousemove/app.js
+++ exercises/week-02/12-dom-events-practice/events/mousemove/app.js
@@ -1,17 +1,20 @@
-// Create global vars for coordinates
-var cursorX;
-var cursorY;
+// Grab container divs
+var xyContainer = document.getElementById("coords")
 
 // Create a container inside the box to display coordinates
-var newSpanX = document.createElement('span')
-var newSpanY = document.createElement('span')
+var newSpanXY = document.createElement('p')
 
 // set eventListener to check if mouse is inside the box
 var box = document.getElementsByClassName('red-box')
-box[0].addEventListener('mouseover', showCoords)
+box[0].addEventListener('mousemove', showCoords)
 
 // Function that displays the coordinates
-function showCoords() {
-    alert('mouseover')
+function showCoords(event) {
+    // prepare the p tags with x y cordinates
+    newSpanXY.textContent = event.pageX + ", " + event.pageY   
     
+    // clear the container of previous results
+    xyContainer.innerHTML = ""
+    // append all of them to the html
+    xyContainer.appendChild(newSpanXY)
 }
