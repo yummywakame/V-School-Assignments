13da4d7ebdacc9ecec1c1832b96c81ebd0c69ea6
Olivia Meiring <olivia@yummy-wakame.com>
2019-02-01 10:48:21 -0700

    fizzbuzz

---
+++ exercises/week-02/16-fizz-buzz/app.js
@@ -0,0 +1,23 @@
+/* 
+    Write a for loop that plays the FizzBuzz game. 
+
+    Your output should count 1 to 100 and replace 
+    everything divisible by 3 with "fizz", everything 
+    divisible by 5 with "Buzz", and everything divisible 
+    by both 3 and 5 with "FizzBuzz"
+*/
+var fizzbuzz = ""
+
+for (var i = 1; i <= 100; i++) {
+    if (!(i % 5) && !(i % 3)) {
+        fizzbuzz = fizzbuzz + "FizzBuzz"
+    } else if (!(i % 5)) {
+        fizzbuzz = fizzbuzz + "fizz"
+    } else if (!(i % 3)) {
+        fizzbuzz = fizzbuzz + "Buzz"
+    } else {
+        fizzbuzz = fizzbuzz + i 
+    }
+
+}
+console.log(fizzbuzz)
