67525eb7b19bc7e37a18d3c69f3ff6d0c58afcc6
Olivia Meiring <olivia@yummy-wakame.com>
2019-02-06 22:06:17 -0700

    complete

--- projects/week-03/business-time/app.js
+++ projects/week-03/business-time/app.js
@@ -1,8 +1,19 @@
 // Show / hide the mobile menu on burger click
-function toggle_visibility(id) {
-    var e = document.getElementById(id);
-    if(e.style.display == 'flex')
-       e.style.display = 'none';
-    else
-       e.style.display = 'flex';
- }
+function toggleMenu(id) {
+   var e = document.getElementById(id);
+   if (e.style.display == 'flex') {
+      e.style.display = 'none';
+      e.style.transition = 'all 0.3s ease';
+   } else {
+      e.style.display = 'flex';
+      e.style.transition = 'all 0.3s ease';
+   }
+}
+
+function fakeFormSubmission(id1, id2) {
+   var id1 = document.getElementById(id1);
+   var id2 = document.getElementById(id2);
+   id1.style.display = 'block';
+   id1.style.transition = 'all 0.3s ease';
+   id2.style.display = 'none';
+}
