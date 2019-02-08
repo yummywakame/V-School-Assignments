198dd59bda49854f5de50bf7b9b3152cbdc769b6
Olivia Meiring <olivia@yummy-wakame.com>
2019-02-08 15:33:36 -0700

    tweaks

--- exercises/week-03/11-array-sort-exercises/app.js
+++ exercises/week-03/11-array-sort-exercises/app.js
@@ -59,3 +59,36 @@ function alphabetical(arr) {
 
 console.log("\n4: Sort an array alphabetically")
 console.log(alphabetical(arr)); // ["by", "dog", "eaten", "family", "wolf"]
+
+
+/* 5) Sort the objects in the array by age */
+
+arr = [
+    {
+        name: "Quiet Samurai",
+        age: 22
+    },
+    {
+        name: "Arrogant Ambassador",
+        age: 100
+    },
+    {
+        name: "Misunderstood Observer",
+        age: 2
+    },
+    {
+        name: "Unlucky Swami",
+        age: 77
+    }
+]
+
+function byAge(arr){
+  return arr.sort(function(a, b) {
+    return (a.age - b.age)
+  })
+}
+
+console.log("\n*** ARRAY:***")
+console.log(arr)
+console.log("\n5: Sort the objects in the array by age")
+console.log(byAge(arr));
