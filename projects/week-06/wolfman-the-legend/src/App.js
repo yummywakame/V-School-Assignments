4ed87ac7868263229e79d9532711852e9a1a36d6
Olivia Meiring <olivia@yummy-wakame.com>
2019-03-01 19:01:21 -0700

    complete

---
+++ projects/week-06/wolfman-the-legend/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import './style.css'
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: []
+        }
+    }
+    
+    getData = () => {
+        axios.get("http://api.icndb.com/jokes/random/")
+        .then(response => {
+            this.setState({ // setState triggers render()
+                data: response.data.value.joke
+            })
+
+        }).catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+    
+    moreWisdom = () => {
+        this.getData()
+    }
+
+    render() {
+        console.log(this.state)
+        let str = this.state.data.toString()
+        str = str.replace(/Chuck Norris'/gi, "Wolfman Steve's")
+        str = str.replace(/Chuck Norris's/gi, "Wolfman Steve's")
+        str = str.replace(/Chuck Norrises/gi, "Wolfman Steve's")
+        str = str.replace(/Chuck Norris/gi, "Wolfman Steve")
+        str = str.replace(/&quot;/gi, "'")
+
+        return (
+            <div id="container">
+                <h1>Wolfman Steve, The Legend</h1>
+                <div id="inner-container">
+                    <div><p>{str}</p></div>
+                    <div><button onClick={this.moreWisdom}>More Lore!</button></div>
+                </div>
+                <a href="https://yummy-wakame.com">Archived by Yummy Wakame &copy; All rights reserved: Wolfman Steve.</a>
+            </div>
+
+        )
+    }
+}
+
+export default App
